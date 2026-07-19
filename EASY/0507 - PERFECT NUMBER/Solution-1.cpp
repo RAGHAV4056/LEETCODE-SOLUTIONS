a1c15b60e932fb6b36fc9efcd47a0414acdfa570
Raghav Sharma <raghavsharma4056@gmail.com>
2026-07-19 17:22:14 +0530

    Delete EASY/0507 - PERFECT NUMBER directory

--- EASY/0507 - PERFECT NUMBER/Solution-1.cpp
+++
@@ -1,9 +0,0 @@
-/*Approach-1 In this taking divisiors till half the original number*/
-class Solution {
-public:
-    bool checkPerfectNumber(int num) 
-    {int total = 0;
-    for(int i=1; i<=num/2; i++)
-        {if(num%i==0){total+=i;}}
-    return total==num;}
-};
